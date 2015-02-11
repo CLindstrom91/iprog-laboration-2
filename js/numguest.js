792c9f81ab1cfb7b5dc5deb03d77d42b98ab540d
kyriaki93 <kyriaki93@gmail.com>
2015-02-11 21:20:54 +0100

    changes

---
+++ js/numguest.js
@@ -0,0 +1,11 @@
+num = 0;
+
+function minus() {
+	num--;
+    document.getElementById('numberOfGuests').innerHTML=num;
+}
+
+function plus() {
+	num++;
+    document.getElementById('numberOfGuests').innerHTML=num;
+}
